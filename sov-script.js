d48b23e3f64ebcb1877522f27d9efd89b0761088
Stephen <stephen.lyssy@gmail.com>
2022-04-02 14:39:00 -0500

    Initial Commit

---
+++ sov-script.js
@@ -0,0 +1,7 @@
+const toggleButton = document.querySelector('.toggle-button');
+const navbarLinks = document.querySelector('.navbar-links');
+
+toggleButton.addEventListener('click', (e) => {
+  e.preventDefault();
+  navbarLinks.classList.toggle('active');
+});
